ef3ada1d7cbf05c72fb8723c8d9772b70faca075
Matumbai Binale <binalematumbai@gmail.com>
2024-08-28 03:10:13 +0300

    (task 2): Add docstring for get_locale

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -20,6 +20,7 @@ babel = Babel(app)
 
 @babel.localeselector
 def get_locale() -> Union[str, None]:
+    """return a users' locale"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
